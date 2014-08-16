c832d5d8222d45d9faadce45222250eca9ac653a
Eddie Ng <wards.ng@gmail.com>
2014-08-15 22:21:38 -0400

    Changed images to resize to 100%

--- app/scripts/controllers/projects.js
+++ app/scripts/controllers/projects.js
@@ -16,7 +16,7 @@ angular.module('rhinoApp')
     for (var i = 0; i < 20; i++) {
       $scope.picts.push({ src: 'http://lorempixel.com/500/250/'});
     }
-    console.log($scope.picts);
+    // console.log($scope.picts);
     // $scope.singers = ['img/shakira.jpg', 'img/justin.jpg', 'img/selena.jpg', 'img/adam.jpg','img/ariana.jpg', 'img/ed.jpg', 'img/harry.jpg', 'img/alicia.jpg', 'img/jennifer.jpg', 'img/blake.jpg', 'img/pink.jpg', 'img/bruno.jpg', 'img/christina.jpg', 'img/jason.jpg',
     //   'img/pitbull.jpg', 'img/katy.jpg', 'img/iggy.jpg'
     // ];
