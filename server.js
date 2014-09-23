92c1de0189147f8263119f9c45169acf705e5117
Adi Sivasankaran <adi.sivasankaran@gmail.com>
2014-09-23 16:51:35 -0700

    Fix bugs.  Ready for first Heroku launch.

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ var logger = require('morgan');
 var path = require('path');
 var app = express();
 
-app.set('views', path.join(__dirname, '/app/views'));
+app.use(express.static(__dirname + '/app'));
+app.set('views', 'app/views');
+
+// app.set('views', path.join(__dirname, '/app/views'));
 app.set('view engine', 'html');
 app.set('view cache', false);
-
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
     // app.use(express.static(path.join(config.root, 'app')));
     
 
-app.use(express.static(__dirname + '/app'));
-app.set('views', '/app/views');
+
 // app.use(express.static(__dirname + '/app/views'));
 
 app.use(logger('dev'));
