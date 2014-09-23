801be3f0c2e6811b74aaea867dc004f78809abb9
Adi Sivasankaran <adi.sivasankaran@gmail.com>
2014-09-23 02:07:27 -0700

    Heroku updates.

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 
 
-// var express = require('express');
-// var logger = require('morgan');
-// var path = require('path');
-// var app = express();
+var express = require('express');
+var logger = require('morgan');
+var path = require('path');
+var app = express();
 
-// app.set('views', path.join(__dirname, '/app/views'));
-// app.set('view engine', 'html');
-// app.set('view cache', false);
+app.set('views', path.join(__dirname, '/app/views'));
+app.set('view engine', 'html');
+app.set('view cache', false);
 
-// app.use(express.static(__dirname + '/app'));
-// // app.use(express.static(__dirname + '/app/views'));
+app.use(express.static(__dirname + '/app'));
+// app.use(express.static(__dirname + '/app/views'));
 
-// app.use(logger('dev'));
+app.use(logger('dev'));
 
-// app.listen(process.env.PORT || 9000);
+app.listen(process.env.PORT || 9000);
 
-// app.get('/', function(req, res){
-// 	res.render('main');
-// });
+app.get('/', function(req, res){
+	res.render('main');
+});
